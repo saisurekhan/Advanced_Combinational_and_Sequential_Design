f1ed3f242723aa11768d3117db8a7a761fcaf5c3
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:16:54 -0500

    Update Compare.v

--- ALU/Compare.v
+++ ALU/Compare.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/14/2025 11:09:24 AM
-// Design Name: 
-// Module Name: Compare
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
