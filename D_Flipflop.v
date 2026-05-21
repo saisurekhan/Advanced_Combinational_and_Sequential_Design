2d61a50bed6f44875ce4126ad185e447205ebfdf
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:09:27 -0500

    Update D_Flipflop.v

--- D_Flipflop.v
+++ D_Flipflop.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/18/2026 01:47:05 PM
-// Design Name: 
-// Module Name: D_Flipflop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module D_Flipflop(
     input wire Data,
     input wire clk,
