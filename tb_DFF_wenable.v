7ebdc4072553e8961ecf133823f8e9defcac3dc5
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:09:36 -0500

    Update tb_DFF_wenable.v

--- tb_DFF_wenable.v
+++ tb_DFF_wenable.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/18/2026 02:49:17 PM
-// Design Name: 
-// Module Name: tb_DFF_wenable
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_DFF_wenable(
     );
