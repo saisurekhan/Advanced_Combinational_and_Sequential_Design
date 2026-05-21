ffd7819fc859912afc62b438bb34f5ffbc325e50
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:09:16 -0500

    Update D_FF_wth_enable.v

--- D_FF_wth_enable.v
+++ D_FF_wth_enable.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/18/2026 02:36:03 PM
-// Design Name: 
-// Module Name: D_FF_wth_enable
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
